33b0ee1b79dbe067586660ae74209da2acdbfe9f
harachan <keishi.hara@pepabo.com>
2025-02-12 09:58:11 +0900

    整理

--- server/scenario_test.go
+++ server/scenario_test.go
@@ -1,24 +1,30 @@
-package main
+package server_test
 
 import (
 	"context"
 	"fmt"
 	"testing"
 
+	"grpc_client_streaming_sample/server"
+
 	"github.com/k1LoW/runn"
 )
 
-func Test_main(t *testing.T) {
-	go func() {
-		main()
-	}()
+func Test_scenario(t *testing.T) {
 	t.Run("", func(t *testing.T) {
 		ctx := context.Background()
 		opts := []runn.Option{
 			runn.T(t),
 			runn.Scopes(runn.ScopeAllowReadParent),
 		}
-		o, err := runn.Load("testdata/scenarios/**/*.yml", opts...)
+		s, err := server.New()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := s.Serve(ctx); err != nil {
+			t.Fatal(err)
+		}
+		o, err := runn.Load("../testdata/scenarios/**/*.yml", opts...)
 		if err != nil {
 			t.Fatal(err)
 		}
